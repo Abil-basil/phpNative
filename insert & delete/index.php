36410a35baec88d4c3f596720f8081787af5a512
Abil-basil <almernabil898@gmail.com>
2025-04-06 01:37:15 +0700

    mengubah tittle di index

--- insert & delete/index.php
+++ insert & delete/index.php
@@ -15,7 +15,7 @@ $mahasiswa = query("SELECT * FROM mahasiswa");
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Document</title>
+	<title>coba php</title>
 </head>
 <body>
 	<h1>daftar mahasiswa</h1>
